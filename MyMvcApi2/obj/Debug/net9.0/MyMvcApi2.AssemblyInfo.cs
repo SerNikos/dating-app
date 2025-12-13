ca20fff83b901b4eed7cadb8aab49b9366d92f80
Christian Culbida <christiancoul@hotmail.gr>
2025-12-13 15:10:13 +0200

    Update build output and project paths
    Updated build artifacts and project configuration to reflect a new local path (from D:\ReactProjects\dating-app\MyMvcApi2 to C:\Users\Christian Culbida\Documents\GitHub\MyMvcApi2). No code or dependency changes were made; only build and environment-specific files were affected.

--- MyMvcApi2/obj/Debug/net9.0/MyMvcApi2.AssemblyInfo.cs
+++ MyMvcApi2/obj/Debug/net9.0/MyMvcApi2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApi2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df5068bce5834a10bf0429f8a1f7197f6288cfe3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1679e362da3d4e9ef809c3c315d00bc619f6aac8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApi2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApi2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
